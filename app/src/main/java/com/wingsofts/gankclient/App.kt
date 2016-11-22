d95dac7a8c6bfab8544e985bf279af9f32047f26
wing <wing@qq.com>
2016-11-22 18:52:30 +0800

    add APP

---
+++ app/src/main/java/com/wingsofts/gankclient/App.kt
@@ -0,0 +1,13 @@
+package com.wingsofts.gankclient
+
+import android.app.Application
+
+/**
+ * Created by wing on 16-11-22.
+ */
+class App :Application(){
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
